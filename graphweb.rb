7f7baca1547ba86b135d55009dd6e72db8eabeb3
Chetan Sarva <chetan@pixelcop.net>
2009-06-11 23:20:15 -0400

    implemented "data stack" concept with web-based console

--- graphweb.rb
+++ graphweb.rb
@@ -54,6 +54,25 @@ require 'pp'
 require 'webrick'
 include WEBrick
 
+def stopwatch(print_time = false)
+    start_time = Time.new
+    yield
+    return if not print_time
+    puts "took %s seconds" % (Time.new - start_time)
+end
+
+class DataServlet < HTTPServlet::AbstractServlet
+
+    def do_GET(req, res)
+    
+#        puts "ajax data request"
+    
+        dataset = DataSet.create
+        res.body = dataset.to_js_table()
+    
+    end
+
+end
 
 class GraphServlet < HTTPServlet::AbstractServlet
 
@@ -67,26 +86,81 @@ class GraphServlet < HTTPServlet::AbstractServlet
         body << <<-EOF
 <html>
   <head>
+    <link rel="stylesheet" media="screen" href="/files/css/graphy.css">
     <script type='text/javascript' src='http://www.google.com/jsapi'></script>
+    <script type='text/javascript' src='/files/js/jquery-1.3.2.min.js'></script>
     <script type='text/javascript'>
       google.load('visualization', '1', {'packages':['annotatedtimeline']});
       google.setOnLoadCallback(drawChart);
       function drawChart() {
         var i;
-        var data = new google.visualization.DataTable(
+        var data_table = new google.visualization.DataTable(
 EOF
         body << datasets.to_js_table()
         body << ", 0.5);\n\n"
 
         body << <<-EOF
         var chart = new google.visualization.AnnotatedTimeLine(document.getElementById('chart_div'));
-        chart.draw(data, {displayAnnotations: true});
+        chart.draw(data_table, {displayAnnotations: true});
+        
+        $("#cli").keypress(function(k) { 
+            if (k.which != 13) {
+                return;
+            }
+            
+            $.ajax({
+                url: "/eval",
+                global: false,
+                type: "POST",
+                data: ({code : $("#cli").val()}),
+                dataType: "html",
+                success: function(msg) {
+                    // add msg to scrollback buffer
+                    result = "<b>" + msg + "</b><br/>"
+                    iframe_body.prepend(result);
+
+                    refresh_chart_data();
+                    
+                }
+            });
+            
+        });
+        
+        function refresh_chart_data() {
+            $.ajax({
+                url: "/data",
+                global: false,
+                type: "GET",
+                dataType: "html",
+                success: function(msg) {
+                    // text is input for new chart
+                    dt_cmd = "new google.visualization.DataTable(" + msg + ", 0.5);";
+                    data_table = eval(dt_cmd);
+                    //input_data = eval(msg);
+                    //data_table = new google.visualization.DataTable(msg, 0.5);
+                    chart.draw(data_table, {displayAnnotations: true});
+                }
+            });        
+        }
+        
+        var iframe_body;
+        
+        $(document).ready(function() {
+            iframe_body = $('body', $('iframe')[0].contentWindow.document);
+            iframe_body.css('font-family', $('iframe').css('font-family'));        
+        });
+        
       }
     </script>
   </head>
 
   <body>
-    <div id='chart_div' style='width: 98%; height: 480px;'></div>
+    <div id='chart_div' style='width: 98%; height: 240px;'></div>
+    
+    <div id="console">
+      <input type="text" id="cli" />
+    </div>
+    <div id="buffer"><iframe id="scrollback"></iframe></div>
   </body>
 </html>
 EOF
@@ -97,10 +171,118 @@ EOF
        
 end
 
+class EvalServlet < HTTPServlet::AbstractServlet
+
+    def do_GET(req, res)
+        do_POST(req, res)
+    end
+
+    def do_POST(req, res)
+    
+        code = req.query['code'].strip
+        i = code.index(' ')
+        if i.nil? then
+            cmd = code
+            args = nil
+        else
+            cmd = code[0, i].strip
+            args = code[i, code.length].strip
+        end
+        
+        puts "got cmd: %s" % cmd
+        puts "got args: %s" % args
+        
+        stack = DataStack.create
+        
+        case cmd    
+        when "pop" 
+#            puts "popping"
+            stack.pop
+        else
+#            puts "pushing command?"
+            if GraphyMath.method_defined? cmd then
+#                puts "yes!"
+                lib = GraphyMath::Lib.new
+                op = lib.method(cmd)
+                proc_cmd = "Proc.new { |data| op.call(data"
+                if args.nil? or args.empty? then
+                    proc_cmd += ") }"
+                else
+                    proc_cmd += (", %s) }" % args)
+                end
+                puts "new op: " + proc_cmd
+                proc = eval(proc_cmd)
+                stack << proc
+            end
+        end
+        
+        res.body = "> " + code + "<br/>"
+        res.body += "ok."
+            
+#         
+#         begin
+#             ret = eval(code)
+#         rescue => ex
+#             res.body = "> " + code + "<br/>not ok. " + ex
+#             return
+#         end
+#         
+#         res.body = "> " + code + "<br/>"
+#         if ret.nil? or not ret then
+#             res.body += "ok."
+#         else
+#             res.body += ("%s" % ret)
+#         end
+        
+    end
+    
+end
+
+class DataStack
+    
+    attr_accessor :ops
+
+    private_class_method :new
+    @@stack = nil
+    def self.create
+        @@stack = new if not @@stack
+        return @@stack
+    end
+    
+    def initialize
+        @ops = []
+    end
+    
+    def <<(op)
+        ops << op
+        dataset = DataSet.create
+        dataset.data = op.call(dataset.data)
+    end
+    
+    def pop
+#        puts "popping"
+        ops.pop
+        process
+    end
+    
+    def process
+#        puts "processing"
+        dataset = DataSet.create
+        dataset.reset
+        return if ops.empty?
+        ops.each { |op|
+#            puts "processing an op.."
+            dataset.data = op.call(dataset.data)
+        }
+        puts "done processing"
+    end
+    
+end
+
 # need this singleton so we can grab the data from inside our servlet
 class DataSet
     
-    attr_accessor :data, :num, :cols    
+    attr_accessor :data, :num, :cols, :orig_data
 
     private_class_method :new
     @@dataset = nil
@@ -110,9 +292,19 @@ class DataSet
     end
     
     def initialize
+        @orig_data = []
         @data = []
         @cols = []
         @num = 0
+    end
+    
+    def set_data(data)
+        @orig_data = @data = data
+    end
+    
+    def reset
+#        puts "resetting dataset"
+        @data = @orig_data
     end
     
     
@@ -129,6 +321,8 @@ class DataSet
     
     def to_js_table
     
+#        puts "to_js_table"
+    
         buff = []
         
         buff << '{ '
@@ -140,6 +334,13 @@ class DataSet
         buff << "],"
         
         buff << 'rows: ['
+        
+        if @data.length > 10000 then
+            # TODO too large! return an empty set for now
+            buff << '] }'
+            return buff.join
+        end
+        
         @data.each_with_index { |d,i|
             buff << "," if i > 0
             buff << sprintf("{c:[{v: new Date(%s)}", d[0] * 1000)
@@ -177,6 +378,12 @@ class DataSet
     end
 
 end
+
+module GraphyMath
+
+    class Lib
+        include GraphyMath
+    end
 
 # variance & std dev swiped from 
 # http://warrenseen.com/blog/2006/03/13/how-to-calculate-standard-deviation/
@@ -285,11 +492,14 @@ def remove_outliers(data, pop, index)
     return data.delete_if { |d| d[index] >= outlier }
 end
 
-def massage_data(data, index, reduce_outliers = true)
+# data = our dataset matrix, usually like [timestamp, col1, col2, col3...]
+# index = col to act on
+# remove_outliers = whether or not to remove points outside 2 std deviations
+def massage_data(data, index, remove_outliers = true)
 
     # remove outliers - anything more than 2 std deviations away
     pop = data.collect { |d| d[index] }
-    data = remove_outliers(data, pop, index) if reduce_outliers
+    data = remove_outliers(data, pop, index) if remove_outliers
     
     # when dealing with multiple datasets, will want to change the scale to 0-100
     # for each set so it graphs nicely
@@ -302,15 +512,25 @@ def massage_data(data, index, reduce_outliers = true)
 
 end
 
+end # module GraphyMath
+
 class Graphy
 
+    include GraphyMath
+
     # currently a file stream works best
-    def load_data(stream)
+    def load_data(filename)
+    
+        print "loading data... "
+        STDOUT.flush
+        
+        stream = File.new(filename)
     
         @datasets = DataSet.create
         
         # setup column info
-        @datasets.cols = [ ['Page size', 'title1', 'text1'], ['Response time', 'title2', 'text2'] ]
+        # [ ['Page size', 'title1', 'text1'], ['Response time', 'title2', 'text2'] ]
+        @datasets.cols = [ ['Page size', '', ''], ['Response time', '', ''] ]
         
         # load csv input
         all_lines = []
@@ -321,13 +541,25 @@ class Graphy
         end
         @datasets.num = all_lines[0].length - 1
         
-        # sort by date (ascending)
-        all_lines.sort! { |a,b| a[0] <=> b[0] }      
+        # sort by date/timestamp (ascending)
+        all_lines.sort! { |a,b| a[0] <=> b[0] }
         
-        @datasets.data = create_rolling_intervals(all_lines, 60)
-        @datasets.data = massage_data(@datasets.data, 1)
-        @datasets.data = massage_data(@datasets.data, 2)
-    
+        @datasets.set_data(all_lines)
+        
+        puts "done"
+        
+        # now use our stacks
+        
+        print "massaging... "
+        STDOUT.flush
+        
+        stack = DataStack.create
+        stack << Proc.new { |data| create_rolling_intervals(data, 60) }
+        stack << Proc.new { |data| massage_data(data, 1) }
+        stack << Proc.new { |data| massage_data(data, 2) }
+        
+        puts "done"
+        
     end
 
     def run
@@ -336,8 +568,12 @@ class Graphy
         puts 'Going to start server at http://localhost:2000/graph'
         puts ''
         
-        s = HTTPServer.new( :Port => 2000, :AccessLog => [ ['/dev/null',AccessLog::COMMON_LOG_FORMAT] ] )
+        s = HTTPServer.new( :Port => 2000, :AccessLog => [ ['/dev/null',AccessLog::COMMON_LOG_FORMAT] ],
+                                           :Logger    => WEBrick::BasicLog.new("/dev/null"))
         s.mount("/graph", GraphServlet)
+        s.mount("/eval", EvalServlet)
+        s.mount("/data", DataServlet)
+        s.mount("/files", HTTPServlet::FileHandler, File.expand_path(File.dirname(__FILE__)), false)
         trap("INT"){ s.shutdown }
         s.start
     
@@ -426,7 +662,9 @@ class GraphyCli
     
     def process_command
         #process_standard_input # [Optional]
-        @graphy.load_data(File.new(@options.file))
+        stopwatch(true) {
+            @graphy.load_data(@options.file)
+        }
         @graphy.run        
         
     end
